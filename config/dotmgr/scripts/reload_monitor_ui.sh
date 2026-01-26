f100947009b571e61eb942d2d4397ea522bec4ce
Chris <chrisoliver345@gmail.com>
2026-01-26 15:10:58 +1300

    Fixed hyprpaper and yazi, switched back to waybar

--- config/dotmgr/scripts/reload_monitor_ui.sh
+++ config/dotmgr/scripts/reload_monitor_ui.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # hyprctl hyprpaper reload ,"$wall_path"
-eww --restart open bar # This is needed to have the eww bar be on top of the wallpaper
+# eww --restart open bar # This is needed to have the eww bar be on top of the wallpaper
